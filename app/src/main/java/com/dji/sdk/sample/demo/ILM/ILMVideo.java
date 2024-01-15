a5f02242b2d5a783f8e7ac4fc60412e85088745f
ibrahim mjadly <48316131+ibrahim3999@users.noreply.github.com>
2024-01-16 00:01:36 +0200

    Update ILMVideo.java

--- app/src/main/java/com/dji/sdk/sample/demo/ILM/ILMVideo.java
+++ app/src/main/java/com/dji/sdk/sample/demo/ILM/ILMVideo.java
@@ -1,31 +1,56 @@
 package com.dji.sdk.sample.demo.ILM;
 
+import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.view.TextureView;
 import android.view.View;
 
+import com.dji.sdk.sample.R;
 import com.dji.sdk.sample.internal.utils.VideoFeedView;
 
+import org.opencv.android.CameraActivity;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+
+import androidx.annotation.NonNull;
 import dji.common.airlink.PhysicalSource;
 import dji.sdk.base.BaseProduct;
-import dji.sdk.camera.Camera;
 import dji.sdk.camera.VideoFeeder;
 import dji.sdk.codec.DJICodecManager;
 import dji.sdk.sdkmanager.DJISDKManager;
-import dji.sdk.sdkmanager.LiveStreamManager;
 
-public class ILMVideo {
+import static android.content.Context.MODE_PRIVATE;
+
+public class ILMVideo extends CameraActivity implements TextureView.SurfaceTextureListener {
+
     private final VideoFeedView videoFeedView;
-    private DJICodecManager djiCodecManager;
+    private Mat grayFrame,rgbFrame,transpose_gray,transpose_rgb;
+    private MatOfRect rects;
+    private CascadeClassifier cascadeClassifier;
+    private DJICodecManager djiCodecManager = null;
+    VideoFeeder.VideoDataListener faceDectection;
     public ILMVideo(VideoFeedView videoFeedView, View coverView) {
         this.videoFeedView = videoFeedView;
         this.videoFeedView.setCoverView(coverView);
+
+
+
     }
 
     public void displayVideo() {
@@ -35,52 +60,125 @@ public class ILMVideo {
             }
         };
         if (VideoFeeder.getInstance() == null) return;
-
         final BaseProduct product = DJISDKManager.getInstance().getProduct();
-
         if (product != null) {
-
-
-            VideoFeeder.VideoDataListener primaryVideoDataListener = videoFeedView.registerLiveVideo(VideoFeeder.getInstance().getPrimaryVideoFeed(), true);
-
-            djiCodecManager = new DJICodecManager(videoFeedView.getContext(), videoFeedView.getSurfaceTexture(), videoFeedView.getWidth(), videoFeedView.getHeight());
-           // djiCodecManager = new DJICodecManager(videoFeedView.getContext(), videoFeedView.getSurfaceTexture(), 0, 0);
-
-
-
+            VideoFeeder.VideoDataListener primaryVideoDataListener =
+                    videoFeedView.registerLiveVideo(VideoFeeder.getInstance().getPrimaryVideoFeed(), true);
             VideoFeeder.getInstance().addPhysicalSourceListener(sourceListener);
 
-            setupFaceDetection();
+        }
 
+        if(product.getCamera().isConnected()){
+            // when the camera init()
+            rgbFrame = new Mat();
+            grayFrame =new Mat();
+
+            rects = new MatOfRect();
+
+            decFace();
+
+            initSDKCallback();
+        }
+
+    }
+    private void decFace() {
+
+            faceDectection =new VideoFeeder.VideoDataListener() {
+                @Override
+                public void onReceive(byte[] bytes, int i) {
+                    rgbFrame = new Mat(i , 1 , CvType.CV_8UC1);   // convert bytes to Mat
+                    rgbFrame.put(0,0,bytes);                    // ...
+
+                    transpose_gray = rgbFrame.t();
+
+                    Imgproc.cvtColor(rgbFrame,grayFrame ,Imgproc.COLOR_RGB2GRAY);
+
+                    transpose_rgb = grayFrame.t();
+
+                    cascadeClassifier.detectMultiScale(transpose_gray,rects,1.1,2);
+
+                    for(Rect rect : rects.toArray()){
+                        // draw rects
+
+                        Mat submat = transpose_gray.submat(rect);
+                        Imgproc.blur(submat,submat,new Size(5,5));
+                        Imgproc.rectangle(transpose_rgb,rect,new Scalar(0,255,0),5);
+
+                        submat.release();
+
+                    }
+                    rgbFrame.get(0,0,bytes);
+
+                    djiCodecManager.sendDataToDecoder(bytes, i);
+                }
+            };
+
+        if(OpenCVLoader.initDebug()){
+            videoFeedView.setEnabled(true);
+            try {
+
+                InputStream inputStream =getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                File file = new File (getDir("cascade",MODE_PRIVATE),"lbpcascade_frontalface.xml");
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+                byte[] data =new byte[4096];
+                int read_bytes=0;
+
+                while ((read_bytes=inputStream.read(data))!=-1){
+                    fileOutputStream.write(data,0,read_bytes);
+                }
+
+                cascadeClassifier = new CascadeClassifier(file.getAbsolutePath());
+
+                if(cascadeClassifier.empty()){
+                    cascadeClassifier = null;
+                }
+
+                inputStream.close();
+                fileOutputStream.close();
+                file.delete();
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
+
+    private void initSDKCallback() {
+        try {
+            VideoFeeder.getInstance().getPrimaryVideoFeed().addVideoDataListener(faceDectection);
+        } catch (Exception ignored) {
+            throw new RuntimeException(ignored);
         }
     }
 
-    private void setupFaceDetection() {
+    @Override
+    public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
+        if (djiCodecManager == null) {
+            djiCodecManager = new DJICodecManager(this,surfaceTexture, i, i1);
+        }
+    }
 
-        CascadeClassifier faceCascade = new CascadeClassifier("app/src/main/res/raw/lbpcascade_frontalface.xml");
+    @Override
+    public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surfaceTexture, int i, int i1) {
 
-        VideoFeeder.VideoDataListener faceDetectionListener = new VideoFeeder.VideoDataListener() {
-            @Override
-            public void onReceive(final byte[] videoBuffer, final int size) {
+    }
 
-                Mat mat = new Mat(size, 1, 1);
-                mat.put(0, 0, videoBuffer);
-                Mat grayMat = new Mat();
-                Imgproc.cvtColor(mat, grayMat, Imgproc.COLOR_RGBA2GRAY);
-                MatOfRect faces = new MatOfRect();
-                faceCascade.detectMultiScale(grayMat, faces);
-                Rect[] facesArray = faces.toArray();
-                for (Rect rect : facesArray) {
-                    Imgproc.rectangle(mat, rect.tl(), rect.br(), new Scalar(255, 0, 0), 2);
-                }
+    @Override
+    public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surfaceTexture) {
+        if (djiCodecManager != null) {
+            djiCodecManager.cleanSurface();
+            djiCodecManager = null;
+        }
+        return false;
+    }
 
-                mat.get(0, 0, videoBuffer);
-                djiCodecManager.sendDataToDecoder(videoBuffer, size);
+    @Override
+    public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surfaceTexture) {
 
-
-            }
-        };
-
-        VideoFeeder.getInstance().getPrimaryVideoFeed().addVideoDataListener(faceDetectionListener);
     }
 }
